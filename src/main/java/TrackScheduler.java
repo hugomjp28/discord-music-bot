095d53c15ff52fbe36b675745f2ed4541ab94376
unknown <hugomjp28@gmail.com>
2023-08-07 18:00:42 +0100

    different lavaplayer library

--- src/main/java/TrackScheduler.java
+++ src/main/java/TrackScheduler.java
@@ -17,7 +17,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class TrackScheduler extends AudioEventAdapter {
     private final BlockingQueue<AudioTrack> queue;
     private boolean loop = false;
-    private AudioPlayer player;
+    private final AudioPlayer player;
     private final AudioPlayerManager playerManager;
 
     public TrackScheduler(AudioPlayer player, AudioPlayerManager playerManager) {
@@ -53,7 +53,7 @@ public class TrackScheduler extends AudioEventAdapter {
                 player.startTrack(null, false);
             }
         } else if(endReason == AudioTrackEndReason.LOAD_FAILED) {
-            //do nothing
+            player.startTrack(queue.poll(), false);
         } else if(endReason != AudioTrackEndReason.REPLACED){
             player.startTrack(null, false);
         }
@@ -72,9 +72,6 @@ public class TrackScheduler extends AudioEventAdapter {
         System.out.println("Track exception: " + exception.toString());
         System.out.println("Exception message: " + exception.getMessage());
         exception.printStackTrace(System.out);
-        player.destroy();
-        this.player = playerManager.createPlayer();
-        //player.startTrack(track,false);
         /*playerManager.loadItem(track.getIdentifier(), new AudioLoadResultHandler() {
             @Override
             public void trackLoaded(AudioTrack audioTrack) {
