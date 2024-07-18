967330de93355025768a46b6f085f2b26dd1e035
hugomjp28 <hugomjp28@gmail.com>
2024-07-18 17:21:00 +0100

    testing ipblock

--- src/main/java/YoutubeAudioManager.java
+++ src/main/java/YoutubeAudioManager.java
@@ -30,8 +30,8 @@ public class YoutubeAudioManager {
         YoutubeAudioSourceManager ytSourceManager = new dev.lavalink.youtube.YoutubeAudioSourceManager(true, true, true, new Client[] { new MusicWithThumbnail(), new WebWithThumbnail(), new AndroidTestsuiteWithThumbnail() });
 
         ArrayList<IpBlock> ipBlocks = new ArrayList<>();
-        Ipv4Block block = new Ipv4Block("161.35.69.0/24");
-        ipBlocks.add(block);
+        Ipv4Block test = new Ipv4Block("192.168.1.0/24");
+        ipBlocks.add(test);
         RotatingIpRoutePlanner routePlanner = new RotatingIpRoutePlanner(ipBlocks);
         YoutubeIpRotatorSetup rotator = new YoutubeIpRotatorSetup(routePlanner);
         rotator.forConfiguration(ytSourceManager.getHttpInterfaceManager(), true)
