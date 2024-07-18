c38eb4f56406b08a42a6e33ae2e3ea9655310369
hugomjp28 <hugomjp28@gmail.com>
2024-07-18 19:10:47 +0100

    revert to ipv4block

--- src/main/java/YoutubeAudioManager.java
+++ src/main/java/YoutubeAudioManager.java
@@ -6,11 +6,9 @@ import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.*;
 import com.sedmelluq.lava.extensions.youtuberotator.YoutubeIpRotatorSetup;
-import com.sedmelluq.lava.extensions.youtuberotator.planner.BalancingIpRoutePlanner;
 import com.sedmelluq.lava.extensions.youtuberotator.planner.RotatingIpRoutePlanner;
 import com.sedmelluq.lava.extensions.youtuberotator.tools.ip.IpBlock;
 import com.sedmelluq.lava.extensions.youtuberotator.tools.ip.Ipv4Block;
-import com.sedmelluq.lava.extensions.youtuberotator.tools.ip.Ipv6Block;
 import dev.lavalink.youtube.YoutubeAudioSourceManager;
 import dev.lavalink.youtube.clients.*;
 import dev.lavalink.youtube.clients.skeleton.Client;
@@ -29,18 +27,16 @@ public class YoutubeAudioManager {
         playerManager = new DefaultAudioPlayerManager();
         AudioSourceManagers.registerRemoteSources(playerManager,
                 com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager.class);
-        YoutubeAudioSourceManager ytSourceManager = new dev.lavalink.youtube.YoutubeAudioSourceManager(true, true, true);
+        YoutubeAudioSourceManager ytSourceManager = new dev.lavalink.youtube.YoutubeAudioSourceManager(true, true, true, new Client[] { new MusicWithThumbnail(), new WebWithThumbnail(), new AndroidTestsuiteWithThumbnail() });
 
         ArrayList<IpBlock> ipBlocks = new ArrayList<>();
-        Ipv6Block ipv6Block = new Ipv6Block("2a03:b0c0:3:d0::1/64");
-        System.out.println(ipv6Block.getRandomAddress());
-        ipBlocks.add(ipv6Block);
-        BalancingIpRoutePlanner balancingIpRoutePlanner = new BalancingIpRoutePlanner(ipBlocks);
-        //RotatingIpRoutePlanner routePlanner = new RotatingIpRoutePlanner(ipBlocks);
-        YoutubeIpRotatorSetup rotator = new YoutubeIpRotatorSetup(balancingIpRoutePlanner);
+        Ipv4Block test = new Ipv4Block("161.35.69.0/24");
+        ipBlocks.add(test);
+        RotatingIpRoutePlanner routePlanner = new RotatingIpRoutePlanner(ipBlocks);
+        YoutubeIpRotatorSetup rotator = new YoutubeIpRotatorSetup(routePlanner);
         rotator.forConfiguration(ytSourceManager.getHttpInterfaceManager(), true)
                 .withMainDelegateFilter(null) // This is important, otherwise you may get NullPointerExceptions.
-                .withRetryLimit(1000)
+                .withRetryLimit(255)
                 .setup();
         playerManager.registerSourceManager(ytSourceManager);
         youtube = playerManager.createPlayer();
